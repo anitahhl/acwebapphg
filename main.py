554977b820dfacd9593d5c7982ea85298cb1becb
Anita Huang <70381898+anitahhl@users.noreply.github.com>
2023-11-23 20:54:41 +0800

    rm piexifx

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from PIL import Image, ImageOps
 from io import BytesIO
-import piexif
 import base64
 import style
 
