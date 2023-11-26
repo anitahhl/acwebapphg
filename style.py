b0f0a49a90151696df8da98049cf080b1813535d
Anita Huang <70381898+anitahhl@users.noreply.github.com>
2023-11-26 13:49:11 +0800

    debug test

--- style.py
+++ style.py
@@ -10,11 +10,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 def load_model(model_path):
     ckpt = torch.load('./static/model.ckpt', map_location=device)
     model = StyleTransferNetwork()
-    print('torch load')
     model.load_state_dict(ckpt['state_dict'])
-    print('load_state_dict')
     model.eval()
-    print('eval')
     return model
 
 
@@ -22,9 +19,11 @@ def stylize(_style_model, style_index, content_image, output_image):
     content_image = utils.imload(content_image, imsize=512)
     style_code = torch.zeros(1, 16, 1)
     style_code[:, style_index, :] = 1
-    
+    print('style_code')
     result = _style_model(content_image, style_code)
+    print('result')
     utils.save_image(output_image, result)
+    print('save image')
 
 
 def get_result(input_image, style_index):
@@ -33,5 +32,5 @@ def get_result(input_image, style_index):
 
     model = load_model(model_path)
     stylize(model, style_index, input_image, output_image)
-
+    print('return output_image')
     return output_image
